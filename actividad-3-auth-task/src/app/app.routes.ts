4cfd95b701f682259b830eb18c3c02c424df688e
TewStylee <jho.roba12@gmail.com>
2025-12-17 22:02:50 -0500

    Auth Guard was implemented for route protection

--- actividad-3-auth-task/src/app/app.routes.ts
+++ actividad-3-auth-task/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { TaskListComponent } from './components/task-list/task-list';
+import { authGuard } from './guards/auth-guard'; 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'tasks', component: TaskListComponent },
+  { 
+    path: 'tasks', 
+    component: TaskListComponent,
+    canActivate: [authGuard] 
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
